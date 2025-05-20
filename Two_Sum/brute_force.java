7ffa85f774e9a6d18b4245295aa7c729e5d9c4e5
SwayamSamparna <swayamsamparna07@gmail.com>
2025-05-20 15:50:16 +0530

    first commit

---
+++ Two_Sum/brute_force.java
@@ -0,0 +1,22 @@
+public class brute_force{
+    public static int[] twoSum(int[] nums, int target){
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] res = twoSum(nums, target);
+        if(res.length==2){
+            System.out.println("Indices: " +  res[0] +", " + res[1]);
+        }else{
+            System.out.println("No valid pair found");
+        }
+    }
+}
